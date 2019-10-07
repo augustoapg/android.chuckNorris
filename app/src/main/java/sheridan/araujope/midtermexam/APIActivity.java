4356052e18778c1807ed018386b08ec716a25ab0
Augusto Goncalez <augustoapg@gmail.com>
2019-10-07 09:49:06 -0400

    api implementation

--- app/src/main/java/sheridan/araujope/midtermexam/APIActivity.java
+++ app/src/main/java/sheridan/araujope/midtermexam/APIActivity.java
@@ -3,12 +3,59 @@ package sheridan.araujope.midtermexam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class APIActivity extends AppCompatActivity {
+
+    private RequestQueue mQueue;
+    private ImageView mImgChuckNorris;
+    private TextView mQuote;
+    private TextView mCreatedAt;
+    private Button mGenerateButton;
+    private Spinner mCategory;
+    private String[] categories = new String[]{"animal", "career", "celebrity", "dev", "explicit",
+            "fashion", "food", "history", "money", "movie", "music", "political", "religion",
+            "science", "sport", "travel"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_api);
+
+        mQueue = Volley.newRequestQueue(this);
+        mImgChuckNorris = findViewById(R.id.imgChuckNorris);
+        mQuote = findViewById(R.id.txtQuote);
+        mCreatedAt = findViewById(R.id.txtCreatedAt);
+        mGenerateButton = findViewById(R.id.btnGenerate);
+        mCategory = findViewById(R.id.categories);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, categories);
+        mCategory.setAdapter(adapter);
+
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCategory.getSelectedItem() != null) {
+                    getQuote();
+                } else {
+                    Toast toast = Toast.makeText(APIActivity.this,
+                            getString(R.string.categoryToast), Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    private void getQuote() {
+
     }
 }
--- app/src/main/java/sheridan/araujope/midtermexam/MainActivity.java
+++ app/src/main/java/sheridan/araujope/midtermexam/MainActivity.java
@@ -2,13 +2,38 @@ package sheridan.araujope.midtermexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mQuotesButton;
+    private Button mMemesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mQuotesButton = findViewById(R.id.btnQuotes);
+        mMemesButton = findViewById(R.id.btnMakeMemes);
+
+        mQuotesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, APIActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mMemesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, APIActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
